9a0b74b6a9252e46911f6c9068262ffa9079eea4
Jorge <jorge.acostaca@campusucc.edu.co>
2023-12-08 13:29:04 -0500

    Se mejora for, agregando un if para que muestre de mejor manera los datos

--- FibonacciSeries/src/main/java/FibonacciSeries.java
+++ FibonacciSeries/src/main/java/FibonacciSeries.java
@@ -1,7 +1,22 @@
 public class FibonacciSeries {
 
-    public static void main(String[] args) {
-        System.out.println("hola");
+   public int GenerarFibonacci(int valorFibonacci){
+    
+        if (valorFibonacci <= 0) {
+            return valorFibonacci;            
+        }
+        
+        int a = 0;
+        int b = 1;
+        
+        for (int i = 2; i <= valorFibonacci; i++) {
+            
+            int aux = a + b;
+            a = b;
+            b = aux;
+        }
+        return b;    
     }
+
     
 }
--- FibonacciSeries/src/main/java/PruebaFibonacci.java
+++ FibonacciSeries/src/main/java/PruebaFibonacci.java
@@ -15,63 +15,57 @@ public class PruebaFibonacci {
 
             try {
 
-                
                 System.out.println();
                 System.out.println("Ingrese la opcion que desee: ");
                 System.out.println("1. Para continuar con el programa");
                 System.out.println("2. Salir");
                 System.out.print("Su opcion es:");
                 int opcion = sc.nextInt();
-                
 
                 switch (opcion) {
                     case 1:
 
+                        System.out.println("");
                         System.out.print("Ingrese la posicion hasta donde desea ver la serie Fibonacci: ");
                         int posicionFibonacci = sc.nextInt();
 
                         System.out.println("La serie Fibonacci hasta la posicion " + posicionFibonacci + " es:");
-                        
-                        if(posicionFibonacci <= 25){
-                        
-                        for (int i = 0; i <= posicionFibonacci; i++) {
-                            System.out.print(Fibonacci.GenerarFibonacci(i) + "   ");
+
+                        for (int i = 0; i < posicionFibonacci; i++) {
+
+                            if (posicionFibonacci <= 26) {
+
+                                System.out.print(Fibonacci.GenerarFibonacci(i) + "   ");
+
+                            } else {
+
+                                System.out.println(Fibonacci.GenerarFibonacci(i) + "   ");
+
+                            }
+
                         }
 
-                        
-                        }else{
-                         
-                            for (int i = 0; i <= posicionFibonacci; i++) {
-                            System.out.println(Fibonacci.GenerarFibonacci(i) + "   ");
-                             }
-                        }
-                      break;
-                        
+                        break;
+
                     case 2:
-                                               
-                    estado = true;      
-                    break;
-                        
+
+                        estado = true;
+                        break;
+
                     default:
-                        System.out.println("***********************************************");
-                        System.out.println("***********************************************");
-                        throw new IllegalArgumentException("Ingrese un valor valido");
+                        System.out.println("");
+                        System.out.println("ERROR!!! INGRESE UN VALOR VALIDO, SOLO 1 ó 2");
+                        break;
                 }
-                   System.out.println();
-                   
+                System.out.println();
+
             } catch (InputMismatchException e) {
-                System.out.println("***********************************************");
-                System.out.println("***********************************************");
-                System.out.println("Ingrese un valor valido");
-                System.out.println("***********************************************");
-                System.out.println("***********************************************");
+                System.out.println("");
+                System.out.println("ERROR!!! INGRESE UN VALOR VALIDO!!!");
                 sc.nextLine();
             } catch (IllegalArgumentException e) {
-                System.out.println("***********************************************");
-                System.out.println("***********************************************");
-                System.out.println("Error en la ejecucion: " + e.getMessage());
-                System.out.println("***********************************************");
-                System.out.println("***********************************************");
+                System.out.println("r");
+                System.out.println(e.getMessage());
                 sc.nextLine();
             }
 
