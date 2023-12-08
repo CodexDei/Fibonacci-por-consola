8ea9bf91de5171933d82b30852c25335ea54c939
Jorge <jorge.acostaca@campusucc.edu.co>
2023-12-08 09:54:41 -0500

    mejora del programa FibonacciSeries.java

--- FibonacciSeries/src/main/java/FibonacciSeries.java
+++ FibonacciSeries/src/main/java/FibonacciSeries.java
@@ -1,22 +1,7 @@
 public class FibonacciSeries {
 
-   public int GenerarFibonacci(int valorFibonacci){
-    
-        if (valorFibonacci <= 0) {
-            return valorFibonacci;            
-        }
-        
-        int a = 0;
-        int b = 1;
-        
-        for (int i = 2; i <= valorFibonacci; i++) {
-            
-            int aux = a + b;
-            a = b;
-            b = aux;
-        }
-        return b;    
+    public static void main(String[] args) {
+        System.out.println("hola");
     }
-
     
 }
